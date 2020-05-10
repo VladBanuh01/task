25b74b62f65df849adbc0c97e8955a74ca047dfd
Vlad Banuh <you@example.com>
2020-05-10 23:03:10 +0300

    print message

--- kek.py
+++ kek.py
@@ -1,2 +1,20 @@
-print('hello World')
+print('Hello Rabotnik!!')
+
+n_one = ""
+n_two = ""
+
+while n_one == "" or n_two == "":
+    try:
+        n_one = int(input("Введите первое положительное число: "))
+        if n_one < 0:
+            raise ValueError("ValueError exception thrown")
+
+        n_two = int(input("Введите второе положительное число: "))
+        if n_two < 0:
+            raise ValueError("ValueError exception thrown")
+
+        # Addition and subtraction
+        print(f"Результат сложения = {n_one+n_two}")
+        print(f"Результат вычитания = {n_one-n_two}") 
+
 
